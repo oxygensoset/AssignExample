6cc21da57d23df4bd185e4ea506e29fcccc5d0b3
oxygensoset <saveliy.burmistrov.99@mail.ru>
2021-11-01 14:35:54 +0300

    T-01AssignExample

---
+++ AssignExample/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            int var1;
+            int var2;
+            var1 = 1488;
+            var2 = (var1 / 4);
+            System.out.println("var1 = " + var1);
+            System.out.println("var2 = var1 / 4 = "+var2);
+
+        }
+    }
+
+
